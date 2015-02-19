84695f26ac084d4cdcbf4f38adceca60c6b79172
Kahlil Lechelt <kahlil.lechelt@etecture.de>
2015-02-19 20:17:03 +0100

    small changes

--- assets/js/src/components/navigation.js
+++ assets/js/src/components/navigation.js
@@ -6,7 +6,9 @@ module.exports = flight.component(navigation, router);
 
 function navigation() {
   this.attributes({
-    navItemSelector: '.navitem'
+    navItemSelector: '.navitem',
+    subnavSelector: '.submenu',
+    activeClassname: 'active'
   });
 
   this.after('initialize', afterInitialize);
@@ -18,12 +20,28 @@ function navigation() {
   }
 
   function onNavClick(event) {
-    var url = $(event.target).attr('href');
+    var target = $(event.target);
+    var url = target.attr('href');
     var data = { url: url };
     var route = URI(url).path();
+    var submenu = target.parents(this.attr.subnavSelector);
+    var activeClassname = this.attr.activeClassname;
 
+    // Remove the active class from all navitems.
+    this.select('navItemSelector').removeClass(activeClassname);
+
+    // If the clicked link is on a submenu set the target to main navitem.
+    if (submenu.length > 0) {
+      target = submenu.prev();
+    }
+
+    // Set active class on the target.
+    target.addClass(activeClassname);
+
+    // Navigate to route.
     this.navigate(route);
-    
+
+    // Prevent default click behavior.
     event.preventDefault();
   }
 }
