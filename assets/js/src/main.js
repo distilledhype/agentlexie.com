a01a8829910329950957295bb38350bc197f958b
Kahlil Lechelt <kahlil.lechelt@etecture.de>
2015-03-10 12:31:05 +0100

    work on player

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -1,9 +1,13 @@
-// Global variables.
+/**
+ * Global variables
+ */
 global.jQuery = global.$ = require('jquery');
 global.Velocity = require('velocity-animate');
 global.Velocity.UI = require('../../../node_modules/velocity-animate/velocity.ui');
 
-// Components.
+/**
+ * Components
+ */
 var Navigation = require('./components/navigation');
 var Pages = require('./components/pages');
 var Routing = require('./components/routing');
--- site/snippets/player.php
+++ site/snippets/player.php
@@ -1,8 +1,8 @@
-<div class="player">
+<div class="player cf">
   <div class="player-progress"><div class="player-progress-indicator"></div></div>
-  <div class="player-controls">
-    <div class="player-play">Pause</div>
-    <div class="player-play">Play</div>
+  <div class="player-controls left">
+    <div class="player-play">▶︎</div>
+    <div class="player-pause">||</div>
   </div>
-  <div class="player-title">Title</div>
+  <div class="player-title left">Title</div>
 </div>
